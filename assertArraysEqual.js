468ae22b68d2d5b2a21bcab6987c3cb915e10332
SalmanHersi <hersi.salman@gmail.com>
2023-07-26 18:41:22 -0400

    optimizations

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const eqArrays = require("./eqArrays");
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Arrays are equal: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🔴🔴🔴 Arrays are not equal: [${array1}] !== [${array2}]`);
+  }
+};
+
+module.exports = assertArraysEqual;
--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,18 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
   }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && !eqArrays(array1[i], array2[i])) {
+      return false;
+    }
+    if (!Array.isArray(array1[i]) && array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
-const isEqual = function (arr1, arr2) {
-  return JSON.stringify(arr1.sort()) == JSON.stringify(arr2.sort());
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-console.log(isEqual([1, 2, 3], [1, 2, 3])); // => true
-console.log(isEqual([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(isEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(isEqual(["1", "2", "3"], ["1", "2", 3])); // => false
+module.exports = eqArrays;

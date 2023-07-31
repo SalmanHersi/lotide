6ad91cf1b7860ccdbbdb51cedaa57d2a44afad46
SalmanHersi <hersi.salman@gmail.com>
2023-07-31 16:43:24 -0400

    refactor

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,10 @@
 const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅ Arrays are equal: [${array1}] === [${array2}]`);
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Arrays are equal: [${arr1}] === [${arr2}]`);
   } else {
-    console.log(`🔴🔴🔴 Arrays are not equal: [${array1}] !== [${array2}]`);
+    console.log(`🔴🔴🔴 Arrays are not equal: [${arr1}] !== [${arr2}]`);
   }
 };
 

a434344a1a24bebc4b6833e00cb230b952518187
SalmanHersi <hersi.salman@gmail.com>
2023-07-31 16:40:08 -0400

    refactor

--- head.js
+++ head.js
@@ -1,3 +1,5 @@
+const assertEqual = require("./assertEqual");
+
 const head = function (arr) {
   if (arr.length === 0) {
     return undefined;
@@ -5,19 +7,4 @@ const head = function (arr) {
   return arr[0];
 };
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([1]), 1);
-assertEqual(head([null, 2, 3]), null);
+module.exports = head;

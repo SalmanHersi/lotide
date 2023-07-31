dc66b7563b68795f28613bb0af8abedebdd4cd84
SalmanHersi <hersi.salman@gmail.com>
2023-07-31 16:44:39 -0400

    refactor

--- middle.js
+++ middle.js
@@ -1,36 +1,36 @@
-const assertArraysEqual = function (array1, array2) {
-  let result = eqArrays(array1, array2);
+// const assertArraysEqual = function (array1, array2) {
+//   let result = eqArrays(array1, array2);
 
-  if (result) {
-    console.log(`✅✅✅  Arrays are equal: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`🔴🔴🔴  Arrays are not equal: [${array1}] !== [${array2}]`);
-  }
-};
+//   if (result) {
+//     console.log(`✅✅✅  Arrays are equal: [${array1}] === [${array2}]`);
+//   } else {
+//     console.log(`🔴🔴🔴  Arrays are not equal: [${array1}] !== [${array2}]`);
+//   }
+// };
 
-const eqArrays = function (array1, array2) {
-  if (array1 === null && array2 === null) {
-    return true;
-  }
-  if (array1 === undefined || array2 === undefined) {
-    if (array1 === array2) {
-      return true;
-    }
-    return false;
-  }
+// const eqArrays = function (array1, array2) {
+//   if (array1 === null && array2 === null) {
+//     return true;
+//   }
+//   if (array1 === undefined || array2 === undefined) {
+//     if (array1 === array2) {
+//       return true;
+//     }
+//     return false;
+//   }
 
-  if (array1.length !== array2.length) {
-    return false;
-  }
+//   if (array1.length !== array2.length) {
+//     return false;
+//   }
 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
+//   for (let i = 0; i < array1.length; i++) {
+//     if (array1[i] !== array2[i]) {
+//       return false;
+//     }
+//   }
 
-  return true;
-};
+//   return true;
+// };
 
 const middleArray = function (array) {
   if (array.length <= 2) {
@@ -46,6 +46,7 @@ const middleArray = function (array) {
   }
 };
 
-assertArraysEqual(middleArray([1]), []);
-assertArraysEqual(middleArray([1, 2]), []);
-assertArraysEqual(middleArray([1, 2, 3]));
+// assertArraysEqual(middleArray([1]), []);
+// assertArraysEqual(middleArray([1, 2]), []);
+// assertArraysEqual(middleArray([1, 2, 3]));
+module.exports = middle;
